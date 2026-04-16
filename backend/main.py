9729128670ee1d557fa8bec99f6d485d9b0bb5c3
Saahir16 <swkhan@asu.edu>
2026-04-15 20:22:45 -0700

    Update main.py by trying out some audit logic

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,8 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from datetime import datetime
+import hashlib
+import json
 
 # Import routers
 from decisions.routes import router as decisions_router
@@ -9,7 +12,7 @@ from decisions.routes import router as decisions_router
 # --------------------------------------------------
 
 app = FastAPI(title="Fintra API")
-
+audit_database = [] # database mock for now to store logs
 # --------------------------------------------------
 # CORS Configuration
 # --------------------------------------------------
@@ -21,9 +24,24 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-# --------------------------------------------------
-# sprint 3 work in progress
-# --------------------------------------------------
+# audit service draft
+class audit_service:
+    @staticmethod
+    def logging_action(user_role: str, action_type: str, module: str, evidence: dict, output: str):
+        timestamp = datetime.now().isoformat() # time of log
+        combined_string = f"{timestamp}-{module}-{output}"
+        id = hashlib.md5(combined_string.encode()).hexdigest()[:8] # keeping logs unique
+        new_log = {
+            "log_id": f"LOG-{id.upper()}",
+            "user": user_role,
+            "time": timestamp,
+            "module": module,      #artifact
+            "action": action_type,
+            "data": evidence,
+            "output": output
+        }
+        audit_database.append(new_log)
+        return new_log
 def compliance_limit(investor_type: str, amount: float):
     if amount < 0:
         return {"signal": "error", "message": "amount cant be negative"}
